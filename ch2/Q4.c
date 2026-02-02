db24cb5a5a37acfeed67183f67c22c633fa43cb4
Anuraag <Anuraag4sun@gmail.com>
2026-02-02 23:54:56 +0530

    Add comment for uninitialized variable program
    Added a comment to describe the purpose of the program.

---
+++ ch2/Q4.c
@@ -0,0 +1,11 @@
+/*
+Write a program that declares several int and float variables—without initializing
+them—and then prints their values. Is there any pattern to the values?
+*/
+#include <stdio.h>
+int main(void)
+{
+  int a , b, c,d;
+    printf("The random garbage values are : %d %d %d %d " ,  a , b ,c , d );
+  return 0;
+}
