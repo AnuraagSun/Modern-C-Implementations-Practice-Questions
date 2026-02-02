3fa027bda4483a7c892d8e508d865b91b6b61fa7
Anuraag <Anuraag4sun@gmail.com>
2026-02-03 00:10:27 +0530

    Add sphere volume calculation program
    Implement program to compute volume of a sphere.

---
+++ ch2/Q6.c
@@ -0,0 +1,17 @@
+/*
+Write a program that computes the volume of a sphere with a 10-meter radius, using the for
+mula v = 4/3πr3. Write the fraction 4/3 as 4.0f/3.0f. (Try writing it as 4/3. What hap
+pens?)
+Hint: C doesn’t have an exponentiation operator, so you’ll need to multiply r by itself
+twice to compute r3
+*/
+
+#include <stdio.h>
+int main(void)
+{
+float r,v;
+printf("Enter r \n");
+scanf("%f", &r);
+v = (4.0f/3.0f)*3.14*r*r*r;
+printf("\n volume is %f", v);
+}
